be2d1a146e1043ec93e3ec45487962be5a4a3f0c
Zehra Akbulut <103004228+zehrakbulut@users.noreply.github.com>
2023-10-23 15:26:42 +0300

    Add project files.

---
+++ MyAcademyCarBook.EntityLayer/Concrete/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAcademyCarBook.EntityLayer.Concrete
+{
+    public class Car
+    {
+        public int CarID { get; set; }
+        public string Model { get; set; }
+
+        public int CarCategoryID { get; set; }
+        public CarCategory CarCategory { get; set; }
+
+        public int BrandID { get; set; }
+        public Brand Brand { get; set; }
+
+        public string ImageUrl { get; set; }
+        public string GearType { get; set; }
+        public int Km { get; set; }
+        public byte PersonCount { get; set; }
+        public int Year { get; set; }
+        public bool Status { get; set; }
+
+        public int CarStatusID { get; set; }
+        public CarStatus CarStatus { get; set; }
+
+        public List<Price> Prices { get; set; }
+    }
+}
---
+++ MyAcademyCarBook.EntityLayer/Concrete/Price.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAcademyCarBook.EntityLayer.Concrete
+{
+    public class Price
+    {
+        public int PriceID { get; set; }
+
+        public int CarID { get; set; }
+        public Car Car { get; set; }
+
+        public string PriceType { get; set; }
+        public decimal PriceValue { get; set; }
+    }
+}
